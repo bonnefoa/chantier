9614ea0791135f6ca4a4247d4ccca4764c460b86
Anthonin Bonnefoy <bonnefoa@poste.isima.fr>
2008-10-28 16:03:51 +0100

    Creation des pages

---
+++ src/main/java/fr/chantier/tapestry/pages/commandes/AffichageCommandes.java
@@ -0,0 +1,11 @@
+package fr.chantier.tapestry.pages.commandes;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sora
+ * Date: Oct 28, 2008
+ * Time: 3:55:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AffichageCommandes {
+}
